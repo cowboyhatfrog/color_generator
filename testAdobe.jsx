cb32ae2163f750c8e0123f2d7e752ab17ae0446c
cowboyhatfrog <146069570+cowboyhatfrog@users.noreply.github.com>
2024-05-22 13:55:47 +0100

    Adobe scripts fro color generator

---
+++ testAdobe.jsx
@@ -0,0 +1,700 @@
+//ALL COLORS
+
+var LAVANDE = new CMYKColor();
+  LAVANDE.cyan = 48;
+  LAVANDE.magenta = 46.67;
+  LAVANDE.yellow = 5.08;
+  LAVANDE.black = 0;
+
+  var PARME = new CMYKColor();
+  PARME.cyan = 15.57;
+  PARME.magenta = 24.23;
+  PARME.yellow = 9.04;
+  PARME.black = 0.05;
+
+  var PURPLE = new CMYKColor();
+  PURPLE.cyan = 82.27;
+  PURPLE.magenta = 88.23;
+  PURPLE.yellow = 19.13;
+  PURPLE.black = 6.51;
+
+  var BRIDALROSE = new CMYKColor();
+  BRIDALROSE.cyan = 0.6;
+  BRIDALROSE.magenta = 34.36;
+  BRIDALROSE.yellow = 18.9;
+  BRIDALROSE.black = 0;
+
+  var GUIMAUVE = new CMYKColor();
+  GUIMAUVE.cyan = 0.75;
+  GUIMAUVE.magenta = 32.69;
+  GUIMAUVE.yellow = 0.19;
+  GUIMAUVE.black = 0;
+
+  var NEONROSE = new CMYKColor();
+  NEONROSE.cyan = 0;
+  NEONROSE.magenta = 75.73;
+  NEONROSE.yellow = 0;
+  NEONROSE.black = 0;
+
+  var SARI = new CMYKColor();
+  SARI.cyan = 1.48;
+  SARI.magenta = 69.92;
+  SARI.yellow = 0;
+  SARI.black = 0;
+
+  var LOTUS = new CMYKColor();
+  LOTUS.cyan = 17.07;
+  LOTUS.magenta = 85.25;
+  LOTUS.yellow = 1.29;
+  LOTUS.black = 0.02;
+
+  var MIA = new CMYKColor();
+  MIA.cyan = 14.99;
+  MIA.magenta = 87.46;
+  MIA.yellow = 0;
+  MIA.black = 0.06;
+
+  var ULTRAVIOLET = new CMYKColor();
+  ULTRAVIOLET.cyan = 45.29;
+  ULTRAVIOLET.magenta = 77.78;
+  ULTRAVIOLET.yellow = 0;
+  ULTRAVIOLET.black = 0;
+
+  var MELBA = new CMYKColor();
+  MELBA.cyan = 0;
+  MELBA.magenta = 23.86;
+  MELBA.yellow = 32.65;
+  MELBA.black = 0;
+
+  var PETALE = new CMYKColor();
+  PETALE.cyan = 2.01;
+  PETALE.magenta = 12.03;
+  PETALE.yellow = 7.09;
+  PETALE.black = 0;
+
+  var PEACH = new CMYKColor();
+  PEACH.cyan = 2.53;
+  PEACH.magenta = 32.91;
+  PEACH.yellow = 38.7;
+  PEACH.black = 0.02;
+
+  var OURO = new CMYKColor();
+  OURO.cyan = 0;
+  OURO.magenta = 44.87;
+  OURO.yellow = 92.5;
+  OURO.black = 0;
+
+  var TONIC = new CMYKColor();
+  TONIC.cyan = 1.46;
+  TONIC.magenta = 12.54;
+  TONIC.yellow = 92.52;
+  TONIC.black = 0;
+
+  var SUN = new CMYKColor();
+  SUN.cyan = 5.51;
+  SUN.magenta = 3.25;
+  SUN.yellow = 39.27;
+  SUN.black = 0;
+
+  var JAUNEFLUO = new CMYKColor();
+  JAUNEFLUO.cyan = 16.05;
+  JAUNEFLUO.magenta = 0;
+  JAUNEFLUO.yellow = 98.31;
+  JAUNEFLUO.black = 0;
+
+  var JELLY = new CMYKColor();
+  JELLY.cyan = 15;
+  JELLY.magenta = 5.26;
+  JELLY.yellow = 68.99;
+  JELLY.black = 0;
+
+  var ORANGEFLUO = new CMYKColor();
+  ORANGEFLUO.cyan = 0;
+  ORANGEFLUO.magenta = 87.53;
+  ORANGEFLUO.yellow = 72.97;
+  ORANGEFLUO.black = 0;
+
+  var ORANGEBDP = new CMYKColor();
+  ORANGEBDP.cyan = 0;
+  ORANGEBDP.magenta = 66.21;
+  ORANGEBDP.yellow = 87.96;
+  ORANGEBDP.black = 0;
+
+  var OASIS = new CMYKColor();
+  OASIS.cyan = 0.04;
+  OASIS.magenta = 45,4;
+  OASIS.yellow = 92.51;
+  OASIS.black = 0.02;
+
+  var NEONORANGE = new CMYKColor();
+  NEONORANGE.cyan = 0;
+  NEONORANGE.magenta = 66.56;
+  NEONORANGE.yellow = 93.88;
+  NEONORANGE.black = 0;
+
+  var SUNSET = new CMYKColor();
+  SUNSET.cyan = 0;
+  SUNSET.magenta = 68.14;
+  SUNSET.yellow = 66.75;
+  SUNSET.black = 0;
+
+  var PUMKIN = new CMYKColor();
+  PUMKIN.cyan = 6.1;
+  PUMKIN.magenta = 71.2;
+  PUMKIN.yellow = 100;
+  PUMKIN.black = 0.54;
+
+  var PEKIN = new CMYKColor();
+  PEKIN.cyan = 0.18;
+  PEKIN.magenta = 99.54;
+  PEKIN.yellow = 97.37;
+  PEKIN.black = 0.2;
+
+  var ROUILLE = new CMYKColor();
+  ROUILLE.cyan = 21.84;
+  ROUILLE.magenta = 99.16;
+  ROUILLE.yellow = 100;
+  ROUILLE.black = 17.63;
+
+  var MARSALA = new CMYKColor();
+  MARSALA.cyan = 30.58;
+  MARSALA.magenta = 99.74;
+  MARSALA.yellow = 76;
+  MARSALA.black = 39.92;
+
+  var RUST = new CMYKColor();
+  RUST.cyan = 28.77;
+  RUST.magenta = 86.13;
+  RUST.yellow = 79.41;
+  RUST.black = 30.93;
+
+  var BRANDY = new CMYKColor();
+  BRANDY.cyan = 28.33;
+  BRANDY.magenta = 88.87;
+  BRANDY.yellow = 92.79;
+  BRANDY.black = 31.21;
+
+  var TUILE = new CMYKColor();
+  TUILE.cyan = 27.39;
+  TUILE.magenta = 75.92;
+  TUILE.yellow = 96.06;
+  TUILE.black = 25.18;
+
+  var AMARANTE = new CMYKColor();
+  AMARANTE.cyan = 37.84;
+  AMARANTE.magenta = 95.85;
+  AMARANTE.yellow = 66.99;
+  AMARANTE.black = 62.17;
+
+  var BURGUNDY = new CMYKColor();
+  BURGUNDY.cyan = 42.02;
+  BURGUNDY.magenta = 87.29;
+  BURGUNDY.yellow = 63.59;
+  BURGUNDY.black = 69.38;
+
+  var COGNAC = new CMYKColor();
+  COGNAC.cyan = 28.59;
+  COGNAC.magenta = 57.13;
+  COGNAC.yellow = 76.73;
+  COGNAC.black = 24.2;
+
+  var CAMEL = new CMYKColor();
+  CAMEL.cyan = 37.84;
+  CAMEL.magenta = 95.85;
+  CAMEL.yellow = 66.99;
+  CAMEL.black = 62.17;
+
+  var MIEL = new CMYKColor();
+  MIEL.cyan = 25.25;
+  MIEL.magenta = 39.94;
+  MIEL.yellow = 61.72;
+  MIEL.black = 14.54;
+
+  var TENT = new CMYKColor();
+  TENT.cyan = 36.18;
+  TENT.magenta = 48.67;
+  TENT.yellow = 89.56;
+  TENT.black = 36.26;
+
+  var BISCUIT = new CMYKColor();
+  BISCUIT.cyan = 16.5;
+  BISCUIT.magenta = 36.7;
+  BISCUIT.yellow = 56.1;
+  BISCUIT.black = 4.89;
+
+  var DUNE = new CMYKColor();
+  DUNE.cyan = 32.32;
+  DUNE.magenta = 41.8;
+  DUNE.yellow = 56.19;
+  DUNE.black = 23.58;
+
+  var DRIEDPETAL = new CMYKColor();
+  DRIEDPETAL.cyan = 30.92;
+  DRIEDPETAL.magenta = 59.36;
+  DRIEDPETAL.yellow = 48.78;
+  DRIEDPETAL.black = 28.24;
+
+  var DESERT = new CMYKColor();
+  DESERT.cyan = 17.6;
+  DESERT.magenta = 30.79;
+  DESERT.yellow = 54.92;
+  DESERT.black = 4.88;
+
+  var SAHARA = new CMYKColor();
+  SAHARA.cyan = 25.73;
+  SAHARA.magenta = 34.63;
+  SAHARA.yellow = 41.76;
+  SAHARA.black = 11.45;
+
+  var MOUTARDE  = new CMYKColor();
+  MOUTARDE .cyan = 16.05;
+  MOUTARDE .magenta = 40.38;
+  MOUTARDE .yellow = 92.59;
+  MOUTARDE .black = 4.71;
+
+  var BELLINI = new CMYKColor();
+  BELLINI.cyan = 0;
+  BELLINI.magenta = 40.16;
+  BELLINI.yellow = 25.29;
+  BELLINI.black = 0;
+
+  var GLAZE = new CMYKColor();
+  GLAZE.cyan = 2.15;
+  GLAZE.magenta = 8.43;
+  GLAZE.yellow = 5.84;
+  GLAZE.black = 0;
+
+  var PAMPLEMOUSSE = new CMYKColor();
+  PAMPLEMOUSSE.cyan = 9.97;
+  PAMPLEMOUSSE.magenta = 41.25;
+  PAMPLEMOUSSE.yellow = 27.05;
+  PAMPLEMOUSSE.black = 0.72;
+
+  var ABSINTHE = new CMYKColor();
+  ABSINTHE.cyan = 56.38;
+  ABSINTHE.magenta = 0;
+  ABSINTHE.yellow = 100;
+  ABSINTHE.black = 0;
+
+  var BRITTANY = new CMYKColor();
+  BRITTANY.cyan = 90.96;
+  BRITTANY.magenta = 41.2;
+  BRITTANY.yellow = 71.82;
+  BRITTANY.black = 47.41;
+
+  var CLAY = new CMYKColor();
+  CLAY.cyan = 43.99;
+  CLAY.magenta = 29.02;
+  CLAY.yellow = 52.08;
+  CLAY.black = 11.64;
+
+  var CYPRUS = new CMYKColor();
+  CYPRUS.cyan = 84.06;
+  CYPRUS.magenta = 82.92;
+  CYPRUS.yellow = 66.94;
+  CYPRUS.black = 47.45;
+
+  var EMERAUDE = new CMYKColor();
+  EMERAUDE.cyan = 88.91;
+  EMERAUDE.magenta = 28.37;
+  EMERAUDE.yellow = 100;
+  EMERAUDE.black = 16.96;
+
+  var LEAF = new CMYKColor();
+  LEAF.cyan = 81.56;
+  LEAF.magenta = 4.95;
+  LEAF.yellow = 91.2;
+  LEAF.black = 0.12;
+
+  var OLIVE = new CMYKColor();
+  OLIVE.cyan = 51.17;
+  OLIVE.magenta = 45.17;
+  OLIVE.yellow = 68.35;
+  OLIVE.black = 47.45;
+
+  var MUD = new CMYKColor();
+  MUD.cyan = 52.71;
+  MUD.magenta = 45.16;
+  MUD.yellow = 61.58;
+  MUD.black = 39.04;
+
+  var KAKI = new CMYKColor();
+  KAKI.cyan = 51.67;
+  KAKI.magenta = 40.9;
+  KAKI.yellow = 53.48;
+  KAKI.black = 29.81;
+
+  var BROWN = new CMYKColor();
+  BROWN.cyan = 34.15;
+  BROWN.magenta = 59.89;
+  BROWN.yellow = 73.43;
+  BROWN.black = 39.75;
+
+  var ALMOND = new CMYKColor();
+  ALMOND.cyan = 16.73;
+  ALMOND.magenta = 20.57;
+  ALMOND.yellow = 27.98;
+  ALMOND.black = 2.09;
+
+  var BABE = new CMYKColor();
+  BABE.cyan = 21.52;
+  BABE.magenta = 35.97;
+  BABE.yellow = 28.95;
+  BABE.black = 5.61;
+
+  var EGGSHELL = new CMYKColor();
+  EGGSHELL.cyan = 3.12;
+  EGGSHELL.magenta = 1.51;
+  EGGSHELL.yellow = 2.93;
+  EGGSHELL.black = 0;
+
+  var EXTRAWHITE = new CMYKColor();
+  EXTRAWHITE.cyan = 0;
+  EXTRAWHITE.magenta = 0;
+  EXTRAWHITE.yellow = 0;
+  EXTRAWHITE.black = 0;
+
+  var CREAM = new CMYKColor();
+  CREAM.cyan = 1.34;
+  CREAM.magenta = 0.91;
+  CREAM.yellow = 1.15;
+  CREAM.black = 0;
+
+  var PURE = new CMYKColor();
+  PURE.cyan = 3.51;
+  PURE.magenta = 3.41;
+  PURE.yellow = 5.27;
+  PURE.black = 0;
+
+  var PEARL = new CMYKColor();
+  PEARL.cyan = 9.2;
+  PEARL.magenta = 5.21;
+  PEARL.yellow = 7.41;
+  PEARL.black = 0;
+
+  var NATURAL = new CMYKColor();
+  NATURAL.cyan = 10.87;
+  NATURAL.magenta = 11.11;
+  NATURAL.yellow = 10.78;
+  NATURAL.black = 0;
+
+  var OXFORDGREY = new CMYKColor();
+  OXFORDGREY.cyan = 28.15;
+  OXFORDGREY.magenta = 20.91;
+  OXFORDGREY.yellow = 20.92;
+  OXFORDGREY.black = 2.82;
+
+  var SABLE = new CMYKColor();
+  SABLE.cyan = 5.36;
+  SABLE.magenta = 11.4;
+  SABLE.yellow = 16.05;
+  SABLE.black = 0;
+
+  var MOONROCK = new CMYKColor();
+  MOONROCK.cyan = 35.31;
+  MOONROCK.magenta = 37.38;
+  MOONROCK.yellow = 34.38;
+  MOONROCK.black = 14.19;
+
+  var CRU = new CMYKColor();
+  CRU.cyan = 7.57;
+  CRU.magenta = 8.11;
+  CRU.yellow = 7.25;
+  CRU.black = 0;
+
+  var GREY = new CMYKColor();
+  GREY.cyan = 45.17;
+  GREY.magenta = 38.14;
+  GREY.yellow = 34.71;
+  GREY.black = 16.01;
+
+  var GRAFITE = new CMYKColor();
+  GRAFITE.cyan = 66.89;
+  GRAFITE.magenta = 57.76;
+  GRAFITE.yellow = 54.67;
+  GRAFITE.black = 60.99;
+
+  var MIDNIGHT = new CMYKColor();
+  MIDNIGHT.cyan = 99.54;
+  MIDNIGHT.magenta = 90.15;
+  MIDNIGHT.yellow = 38.74;
+  MIDNIGHT.black = 42.27;
+
+  var BLACK = new CMYKColor();
+  BLACK.cyan = 91.17;
+  BLACK.magenta = 78.69;
+  BLACK.yellow = 61.95;
+  BLACK.black = 97.45;
+
+  var COBALT = new CMYKColor();
+  COBALT.cyan = 100;
+  COBALT.magenta = 89.96;
+  COBALT.yellow = 39.55;
+  COBALT.black = 36.66;
+
+  var MIDNIGHTMOKKA = new CMYKColor();
+  MIDNIGHTMOKKA.cyan = 100;
+  MIDNIGHTMOKKA.magenta = 88.95;
+  MIDNIGHTMOKKA.yellow = 49.38;
+  MIDNIGHTMOKKA.black = 70.63;
+
+  var INDIGO = new CMYKColor();
+  INDIGO.cyan = 100;
+  INDIGO.magenta = 86.48;
+  INDIGO.yellow = 20.27;
+  INDIGO.black = 3.07;
+
+  var PAROS = new CMYKColor();
+  PAROS.cyan = 92.74;
+  PAROS.magenta = 75.3;
+  PAROS.yellow = 0;
+  PAROS.black = 0;
+
+  var CALIFORNIA = new CMYKColor();
+  CALIFORNIA.cyan = 68.02;
+  CALIFORNIA.magenta = 50.76;
+  CALIFORNIA.yellow = 37.51;
+  CALIFORNIA.black = 24.8;
+
+  var TARMAC = new CMYKColor();
+  TARMAC.cyan = 90.29;
+  TARMAC.magenta = 53.55;
+  TARMAC.yellow = 39.91;
+  TARMAC.black = 30.27;
+
+  var ICE = new CMYKColor();
+  ICE.cyan = 21.5;
+  ICE.magenta = 6.86;
+  ICE.yellow = 10.43;
+  ICE.black = 0;
+
+  var FROST = new CMYKColor();
+  FROST.cyan = 8.16;
+  FROST.magenta = 0.76;
+  FROST.yellow = 6.38;
+  FROST.black = 0;
+
+  var MENTHOL = new CMYKColor();
+  MENTHOL.cyan = 13.2;
+  MENTHOL.magenta = 2.18;
+  MENTHOL.yellow = 6.91;
+  MENTHOL.black = 0;
+
+  var MATCHA = new CMYKColor();
+  MATCHA.cyan = 25.68;
+  MATCHA.magenta = 11.11;
+  MATCHA.yellow = 21.67;
+  MATCHA.black = 0.36;
+
+  var JADE = new CMYKColor();
+  JADE.cyan = 19.25;
+  JADE.magenta = 7.31;
+  JADE.yellow = 20.2;
+  JADE.black = 0.01;
+
+  var ROCK = new CMYKColor();
+  ROCK.cyan = 34.03;
+  ROCK.magenta = 38.9;
+  ROCK.yellow = 54.23;
+  ROCK.black = 22.04;
+
+  var STEEL = new CMYKColor();
+  STEEL.cyan = 42.31;
+  STEEL.magenta = 13.31;
+  STEEL.yellow = 15.71;
+  STEEL.black = 0.23;
+
+  var TURQUOISE = new CMYKColor();
+  TURQUOISE.cyan = 63.35;
+  TURQUOISE.magenta = 0;
+  TURQUOISE.yellow = 24.55;
+  TURQUOISE.black = 0;
+
+
+  var PIERRE = new CMYKColor();
+  PIERRE.cyan = 6.32;
+  PIERRE.magenta = 5.77;
+  PIERRE.yellow = 12.54;
+  PIERRE.black = 0;
+
+  var BUTTER = new CMYKColor();
+  BUTTER.cyan = 3.03;
+  BUTTER.magenta = 12.72;
+  BUTTER.yellow = 40.86;
+  BUTTER.black = 0;
+
+
+  
+
+  var nuancier = [LAVANDE, PARME, PURPLE, BRIDALROSE, GUIMAUVE, NEONROSE, SARI, LOTUS, MIA, ULTRAVIOLET,
+    PETALE, MELBA, PEACH, OURO, PIERRE, BUTTER, TONIC, SUN, JAUNEFLUO, ORANGEFLUO, ORANGEBDP, OASIS,
+    NEONORANGE, SUNSET, PUMKIN, PEKIN, ROUILLE, MARSALA, RUST, BRANDY, TUILE, AMARANTE, BURGUNDY,
+    COGNAC, CAMEL, MIEL, TENT, BISCUIT, DUNE, DRIEDPETAL, DESERT, SAHARA, MOUTARDE, BELLINI, GLAZE,
+    PAMPLEMOUSSE, ABSINTHE, BRITTANY, CLAY, CYPRUS, EMERAUDE, LEAF, OLIVE, MUD, KAKI, BROWN, ALMOND,
+    BABE, EGGSHELL, EXTRAWHITE, CREAM, PURE, PEARL, NATURAL, OXFORDGREY, SABLE, MOONROCK, CRU, GREY,
+    GRAFITE, MIDNIGHT, COBALT, BLACK, MIDNIGHTMOKKA, INDIGO, PAROS, CALIFORNIA, TARMAC, ICE, FROST,
+    MATCHA, MENTHOL, JADE,ROCK, STEEL, TURQUOISE];
+
+  var violets = [LAVANDE, PARME, PURPLE, LOTUS, MIA, ULTRAVIOLET];
+  var oranges = [MELBA, PEACH, OURO, BUTTER, TONIC, SUN, JAUNEFLUO, ORANGEFLUO, ORANGEBDP, OASIS,
+     NEONORANGE, SUNSET, PUMKIN, MOUTARDE ];
+  var roses = [GUIMAUVE, BRIDALROSE, NEONROSE, SARI, LOTUS, MIA, PETALE, DRIEDPETAL, BELLINI, GLAZE, PAMPLEMOUSSE, BABE];
+  var rouges = [PEKIN, ROUILLE, MARSALA, RUST, BRANDY, AMARANTE, BURGUNDY];
+  var marrons = [RUST,BRANDY, TUILE, AMARANTE, BURGUNDY, COGNAC, CAMEL, MIEL, TENT, BROWN];
+  var beiges = [MIEL, BISCUIT, DUNE, DESERT, SAHARA, ALMOND, SABLE, ROCK];
+  var verts = [ABSINTHE, BRITTANY, CLAY, CYPRUS, EMERAUDE, LEAF, OLIVE, MUD, KAKI, MATCHA, JADE];
+  var blancs = [EGGSHELL, EXTRAWHITE,CREAM, PURE, CRU];
+  var gris = [PEARL, NATURAL, OXFORDGREY, MOONROCK, GREY, GRAFITE];
+  var fonces = [GRAFITE, MIDNIGHT, BLACK, COBALT, MIDNIGHTMOKKA];
+  var bleus = [MIDNIGHT, COBALT, INDIGO, PAROS, CALIFORNIA, TARMAC, ICE, FROST, MENTHOL, STEEL, TURQUOISE];
+
+  var pastels = [ICE, MELBA, PEACH, GLAZE, SUN, ALMOND, SAHARA, PARME, PETALE];
+
+  var couleursATester = [ICE, KAKI, LOTUS, BELLINI, OASIS, ORANGEBDP, DESERT];
+  var setDeChoix = [KAKI, DESERT];
+    
+    
+var doc = app.activeDocument;
+
+//Si semelle à colorer choisir j = 1
+//Si semelle butter choisir j = -1
+//Si semelle pierre choisir j = 0
+
+var j = 0;
+  
+
+  
+  //créé un nouveau calque
+  var newLayer = doc.layers.add();
+  newLayer.name = "color test";
+
+  var i =0;
+  var k=0;
+
+
+  while(k<2){
+
+  while(i < 701){
+
+  var languette = doc.pathItems.add();
+  languette.name = "languette" + i;
+  languette.setEntirePath(Array([221 +235*k, 994 -i], [225+235*k, 995-i], [250+235*k, 975-i], [246+235*k, 967-i], [219+235*k, 968-i], [214+235*k, 981-i], [221+235*k, 994-i]));
+  languette.close = true;
+  languette.filled= true;
+  languette.stroked= false;
+  //languette.fillColor = couleursATester[Math.floor(Math.random() * couleursATester.length)];
+  languette.fillColor = DESERT;
+
+  var face = doc.pathItems.add();
+  face.name = "face" +i;
+  //create pairs of coordinates for every anchor point
+  face.setEntirePath(Array([180+235*k, 968 -i], [189+235*k, 964-i], [207+235*k, 967-i], [223+235*k, 977-i], [277+235*k, 952-i], [284+235*k, 928-i], [260+235*k, 923-i], [206+235*k, 930-i], [190+235*k, 934-i], [173+235*k, 957-i], [180+235*k, 968-i]));
+  face.close = true;
+  face.filled= true;
+  face.stroked= false;
+  face.fillColor = couleursATester[Math.floor(Math.random() * couleursATester.length)];
+
+  var semelle = doc.pathItems.add();
+semelle.name = "semelle"  +i;
+semelle.setEntirePath(Array([155+235*k, 936-i], [225+235*k, 930-i], [246+235*k, 923-i], [342+235*k, 930-i], [346+235*k, 918-i], [333+235*k, 913-i], [282+235*k, 904-i], [249+235*k, 906-i], [157+235*k, 909-i], [155+235*k, 936-i]));
+semelle.close = true;
+semelle.filled= true;
+semelle.stroked= false;
+
+if(j>0){
+    semelle.fillColor = nuancier[Math.floor(Math.random() * nuancier.length)];
+}else if(j<0){
+    semelle.fillColor = BUTTER;
+}else{
+    semelle.fillColor = PIERRE;
+}
+
+var trous = doc.pathItems.add();
+trous.name = "trous" +i;
+trous.setEntirePath(Array([216+235*k, 970-i], [226+235*k, 978-i], [250+235*k, 975-i], [267+235*k, 964-i], [301+235*k, 951-i], [295+235*k, 945-i], [276+235*k, 950-i], [235+235*k,968-i], [223+235*k,967-i],[216+235*k,970-i]));
+trous.close = true;
+trous.filled= true;
+trous.stroked= false;
+trous.fillColor = couleursATester[Math.floor(Math.random() * couleursATester.length)];
+
+var lowerFront = doc.pathItems.add();
+lowerFront.name = "lower front" +i;
+lowerFront.setEntirePath(Array([275+235*k, 950-i], [285+235*k, 946-i], [293+235*k, 934-i], [326+235*k, 940-i], [331+235*k, 944-i], [341+235*k, 939-i], [346+235*k, 932-i], [299+235*k,922-i], [260+235*k,923-i],[272+235*k,936-i], [275+235*k,944-i], [275+235*k,950-i]));
+lowerFront.close = true;
+lowerFront.filled= true;
+lowerFront.stroked= false;
+lowerFront.fillColor = couleursATester[Math.floor(Math.random() * couleursATester.length)];
+
+var upperFront = doc.pathItems.add();
+upperFront.name = "upper front" +i;
+upperFront.setEntirePath(Array([286+235*k, 947-i], [296+235*k, 946-i], [300+235*k, 948-i], [302+235*k, 951-i], [317+235*k, 947-i], [331+235*k, 944-i], [326+235*k, 941-i], [314+235*k,940-i], [305+235*k,940-i], [295+235*k,935-i], [288+235*k, 940-i], [287+235*k,947-i]));
+upperFront.close = true;
+upperFront.filled= true;
+upperFront.stroked= false;
+upperFront.fillColor = couleursATester[Math.floor(Math.random() * couleursATester.length)];
+
+  var V = doc.pathItems.add();
+  V.name = "V" +i;
+// create pairs of coordinates for every anchor point
+V.setEntirePath(Array([225+235*k, 962-i], [235+235*k,956-i], [231+235*k, 931-i], [250+235*k, 947-i], [260.6+235*k, 943-i], [243+235*k, 926-i], [218+235*k, 927-i], [225+235*k, 962-i]));
+V.close = true;
+V.filled= true;
+V.stroked= false;
+//V.fillColor =  couleursATester[Math.floor(Math.random() * couleursATester.length)];
+V.fillColor = OASIS;
+
+var back = doc.pathItems.add();
+back.name = "back" +i;
+back.setEntirePath(Array([160+235*k, 965-i], [168+235*k, 968-i], [180+235*k, 963-i], [203+235*k, 945-i], [207+235*k, 937-i], [206+235*k, 930-i], [206+235*k, 931-i], [155+235*k, 935-i], [156+235*k, 948-i], [155+235*k, 936-i]));
+back.close = true;
+back.filled= true;
+back.stroked= false;
+back.fillColor = setDeChoix[Math.floor(Math.random() * setDeChoix.length)];
+
+
+var upperBack = doc.pathItems.add();
+upperBack.name = "upper back" +i;
+upperBack.setEntirePath(Array([162+235*k, 987-i], [163+235*k, 988-i], [170+235*k, 985-i], [173+235*k, 975-i], [180+235*k, 968-i], [175+235*k, 962-i], [159+235*k, 965-i], [162+235*k, 987-i]));
+upperBack.close = true;
+upperBack.filled= true;
+upperBack.stroked= false;
+upperBack.fillColor = couleursATester[Math.floor(Math.random() * couleursATester.length)];
+
+i=i+100;
+
+  }
+  k=k+1;
+  i=0;
+}
+
+
+  
+ /* var newPathItem = newLayer.pathItems.add();
+  newPathItem.filled = true;
+  newPathItem.fillColor = LAVANDE;
+  var pathPoints = [];
+  var pathPoint;
+  
+  var vertices = [[207, -900], [231, -915], [213, -930], [231, -915], [240, -922], [224, -937], [200, -935]];
+  
+  for(var i = 0; i < vertices.length; i++) {
+      pathPoint = newPathItem.pathPoints.add();
+      pathPoint.anchor = vertices[i];
+      pathPoints.push(pathPoint);
+      }
+  newPathItem.pathPoints = pathPoints; */
+
+  //  var svgFile = File(Folder.desktop + '/v-10 vectorisé.svg');
+   // doc.groupItems.createFromFile(svgFile);
+
+  /* var currentLayer = doc.layers[0];
+  var pathItems = currentLayer.pathItems;
+  var pathItem = pathItems[0];
+  var points = pathItem.pathPoints;
+  */
+  
+  /* read points
+  for(var i = 0; i < points.length; i++) {
+  $.writeln(points[i].anchor);
+  }
+  */
